8f63054130606470d8877820e032211c0cce3e4e
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-06-15 23:12:48 +0800

    update main.py 1st page scraper

--- efc_jobs/main.py
+++ efc_jobs/main.py
@@ -7,6 +7,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 class jobScrape():
 
@@ -23,20 +24,54 @@ class jobScrape():
 
     def job_card(self):
         soup = self.parser()
-        # soup.find("div", {"class": "job-category"}).find_all("a")
-        efc_jobcard = soup.find("efc-job-card", {"class": "w-100 job-card ng-star-inserted"})
-        card_details = efc_jobcard.find_all("efc-card-details")
-        #card_footer = efc_jobcard.find_all("efc-card-footer")
+    
+        efc_jobcard = soup.find('div', {'class': 'd-flex flex-column jobs-container ng-star-inserted'}).find_all('efc-job-card')
 
+        card_details = [card.find('efc-card-details') for card in efc_jobcard]
+        card_footer = [card.find('efc-card-footer') for card in efc_jobcard]
         
+        return card_details, card_footer
+    
 
-        return card_details
+    def first_page(self):
+        jobcard = self.job_card()
+        card_det, card_ftr = jobcard[0], jobcard[1]
+
+        job_title, company, job_link, location, position_type, work_arrangement, salary, posted_on = [], [], [], [], [], [], [], []
+        job_dct = {}
+
+        for item in card_det:
+            job_title.append(item.find('a').get('title'))
+            company.append(item.find_all('div')[3].text.lstrip().rstrip())
+            job_link.append(item.find('a').get('href'))
+            location.append(item.find('span' , {'class': 'dot-divider'}).text)
+            position_type.append(item.find('span' , {'class': 'dot-divider ng-star-inserted'}).text)
+        
+        for item in card_ftr:
+            ln1 = len(item.find_all('div')[1])
+            ln2 = len(item.find_all('div')[1].find_all('span'))
+        
+            if ln1 == 6 and ln2 == 2:
+                work_arrangement.append(item.find_all('div')[1].find_all('span')[0].text)
+                salary.append(item.find_all('div')[1].find_all('span')[1].text)
+            elif ln1 == 5 and ln2 == 1:
+                work_arrangement.append(None)
+                salary.append(item.find_all('div')[1].find_all('span')[0].text)
+
+            posted_on.append(item.find('efc-job-meta').text.lstrip().rstrip())
+        
+        job_dct = {"Position": job_title, "Company": company, "Link": job_link, "Location": location, "Position Type": position_type, "Work Arrangement": work_arrangement, "Salary": salary, "Posted On": posted_on}
+        final_df = pd.DataFrame(job_dct)
+
+        return final_df
+
+
+
 
 
 if __name__ == '__main__':
     efc_url = "https://www.efinancialcareers.co.uk/jobs/in-United-Kingdom" 
 
-    test = jobScrape(efc_url).job_card()
+    test = jobScrape(efc_url).first_page()
 
-    for i in test:
-        print(f"{i}\n")
+    print(test)
